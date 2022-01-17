38e9cc54bf1270edadcf17fa5ff03daaf9b87d05
Vlad <example@gmail.com>
2022-01-17 19:59:45 +0300

    add cycle

--- js/script.js
+++ js/script.js
@@ -10,12 +10,51 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-    b = prompt("На сколько оцените его?", ""),
-    c = prompt("Один из последних просмотренных фильмов?", ""),
-    d = prompt("На сколько оцените его?", "");
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt("Один из последних просмотренных фильмов?", ""),
+//         b = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+//     if (a != null && b != null && a != '' && b != '' && a.length <= 50 && b.length <= 50) {
+//         personalMovieDB.movies[a] = b;
+//     } else {
+//         i--;
+//     }
+// }
+
+// let i = 0;
+// while (i < 2) {
+//     const a = prompt("Один из последних просмотренных фильмов?", ""),
+//         b = prompt("На сколько оцените его?", "");
+
+//     if (a != null && b != null && a != '' && b != '' && a.length <= 50 && b.length <= 50) {
+//         personalMovieDB.movies[a] = b;
+//     } else {
+//         i--;
+//     }
+//     i++;
+// }
+
+let i = 0;
+do {
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+        b = prompt("На сколько оцените его?", "");
+
+    if (a != null && b != null && a != '' && b != '' && a.length <= 50 && b.length <= 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
+    i++;
+} while (i < 2);
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довльно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
